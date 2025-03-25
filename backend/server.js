d3d0a06b79334ffdac017c74416e936f69c1693a
Dario Simpson <20348771+dariusbarbus@users.noreply.github.com>
2025-03-25 18:29:33 -0400

    update server port

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const fs = require('fs');
 const path = require('path');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Enable CORS for all origins
 app.use(cors());
